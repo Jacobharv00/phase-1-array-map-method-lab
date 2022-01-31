56997c223d181be5c2902a2d25302d97460042dc
jacob <jacobharv00@gmail.com>
2022-01-31 12:38:52 -0700

    setup different ways to solve titleCased()

--- index.js
+++ index.js
@@ -11,18 +11,63 @@ const tutorials = [
   'what is JSONP?'
 ]
 
+// Notes
+// First: map() over the tutorials array and to look at each tutorial
+// Second: split(' ') on a space to make an array of strings of each word from each tutorial
+// Third: make a nested map() method to iterate over each word so that we can modify each word to be correctly titleCased. 
+// Fourth: How can we get the first character of each word? => word.charAt(0)
+// Fifth: How can we capitalize that first letter? Can we chain onto the last method? 
+// => .toUpperCase(), YES!
+// Sixth: How can we get the rest of the word after capitalizing the first letter.
+// concat word.slice(1) => get remainder of string (from the second character to the end of the string)
+// Seventh: How can we now take these words and put them back into an array of strings?
+// using .join(' ') method to join on a space
+// what you pass .join('') will determine the format returned
+
 
 const titleCased = () => {
 
   return tutorials.map( tutorial => {
-    const letters = tutorial.split( ' ' )
+    // Turns into array of strings
+    const wordsArr = tutorial.split( ' ' )
 
-    const capitalizedLetters = letters.map( letter => (
-      letter.charAt( 0 ).toUpperCase() + letter.slice( 1 )
-    ) )
+    const capitalized = wordsArr.map( word => {
+      return word.charAt( 0 ).toUpperCase() + word.slice( 1 )
+    } )
 
-    const capitalizedStrings = capitalizedLetters.join( ' ' )
-    return capitalizedStrings
+
+    // const capitalizedStrings = capitalized.join( ' ' )
+    // return capitalizedStrings
+
+    // Turns back into individual strings inside array
+    return capitalized.join( ' ' )
 
   } )
 }
+
+titleCased()
+
+
+
+const people = [ 'Justin', 'jACob', 'grAcIe', 'natasha', 'emiLY' ]
+
+// First Way
+const titleCased = () => {
+  return people.map( person => {
+    return person.charAt( 0 ).toUpperCase() + person.slice( 1 ).toLowerCase()
+  } )
+}
+
+titleCased()
+
+
+// Second Way
+const titleCased = () => {
+  const capitalizedPeople = people.map( person => {
+    return person.charAt( 0 ).toUpperCase() + person.slice( 1 ).toLowerCase()
+  } )
+  console.log( capitalizedPeople )
+  //return capitalizedPeople
+}
+
+titleCased()
